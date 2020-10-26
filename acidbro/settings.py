31aa0bc1e4376eb03745a1cbc3a0fc1774cedb9a
Dmitry Danilov <danilovdmitry94@gmail.com>
2020-10-26 21:39:50 +0200

    Reinitialize project. update requirements

--- acidbro/settings.py
+++ acidbro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r86xar9wu#gxmn-h**x*o)(m&i!)oqdp9(zykmz0q(rpi@pvy)'
+SECRET_KEY = '48v^0&5xkk8x)1q5awew$8iqmw_)*$3=7oxrv)-$g*(_0f$j2#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
