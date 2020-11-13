ca140d0e5c8753cf42b2580dde9575e5f38ea4ff
Dmitry Danilov <danilovdmitry94@gmail.com>
2020-11-13 07:33:38 +0200

    Provide Seo abstract model

--- seo/admin.py
+++ seo/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+
+class SeoAdminMixin(object):
+    """
+    Mixin класс для разделения сео данных в админ панели
+    """
+    def get_fieldsets(self, request, obj=None):
+        seo_fields = ['page_title_en', 'page_title_ru', 'page_keywords_en',
+                      'page_keywords_ru', 'page_description_en',
+                      'page_description_ru']
+        if self.fieldsets:
+            return self.fieldsets
+        fields = [
+            x for x in self.get_fields(request, obj) if not x in seo_fields
+        ]
+        return [
+            (None, {'fields': fields}), ('SEO', {'fields': seo_fields})
+        ]
--- seo/models.py
+++ seo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class SeoMeta(models.Model):
+    """
+    Абстрактный класс сео мета данных
+    """
+    page_title = models.CharField(
+        _(u'Заголовок страницы (SEO/Meta)'),
+        max_length=350,
+        blank=True,
+    )
+    page_keywords = models.CharField(
+        _(u'Ключевые слова страницы (SEO/Meta)'),
+        max_length=500,
+        blank=True
+    )
+    page_description = models.CharField(
+        _(u'Описание страницы (SEO/Meta)'),
+        max_length=1000,
+        blank=True
+    )
+
+    class Meta:
+        abstract = True
+
