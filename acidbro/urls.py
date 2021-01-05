f6c8ec59b8442c571a20a766ccfd081921f0230d
Dmitry Danilov <danilovdmitry94@gmail.com>
2021-01-05 20:37:46 +0200

    Fix static and media urls

--- acidbro/urls.py
+++ acidbro/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
+from django.views.static import serve
+
 from pages.views import TemplateAdminAJAXView, IndexView
 
 
@@ -30,7 +31,9 @@ urlpatterns = i18n_patterns(
          TemplateAdminAJAXView.as_view()),
 
     path('', include('pages.urls')),
-)
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
+)
